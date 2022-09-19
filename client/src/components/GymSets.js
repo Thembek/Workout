37aec8853d039f321f657e66103d6943e4599b01
Andile <andile.goba@younglings.africa>
2022-09-19 14:48:21 +0200

    for beginners label update commi

--- client/src/components/GymSets.js
+++ client/src/components/GymSets.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import style from 'styled-components';
+
+const Name = style.a`
+    text-decoration: none;
+    &:hover{
+        color: #009bff;
+    }
+`;
 
 function GymSets(){
     return (
         <div className="gym-sets">
-            <Link to="/notes">For Beginners</Link>
+            <Link to="/notes">
+                <Name>
+                    For Beginners
+                </Name>
+            </Link>
             <div className="gym-details">
                 <h1 to="/notes">
                     workouts you might need to know
